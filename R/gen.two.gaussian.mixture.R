3b842d8b220f01d8f6bf0ecb885a1777d2048f31
John Minter <jrminter@gmail.com>
2016-07-09 09:24:36 -0400

    first function added. passes check.

---
+++ R/gen.two.gaussian.mixture.R
@@ -0,0 +1,44 @@
+#' Generate a sample from a two Gaussian mixture
+#' 
+#' @param n - the number of samples to generate
+#' 
+#' @param p1 - the fractiona probability of the first Gaussian (< 1.0)
+#' 
+#' @param mu1 - the mean of the first Gaussian
+#' 
+#' @param s1 - the standard deviation of the first Gaussian
+#' 
+#' @param mu2 - the mean of the second Gaussian
+#' 
+#' @param s2 - the standard deviation of the second Gaussian
+#' 
+#' @param rn.seed - the random number seed (default 42)
+#' 
+#' @return rand.samples - a vector of n samples
+#' 
+#' @importFrom stats rnorm runif
+#' 
+#' @export
+#' 
+#' @examples
+#' # library(pkgname)
+#' samples <- gen.two.gaussian.mixture(10000, 0.5, 0.5, 1.0, 10.0, 3.0)
+#' 
+#' @export
+gen.two.gaussian.mixture <- function(n, p1, mu1, s1, mu2, s2, rn.seed=42){
+  set.seed(rn.seed)
+  #Sample N random uniforms U
+  U <- runif(n)
+  #Variable to store the samples from the mixture distribution
+  rand.samples <- rep(NA, n)
+  
+  #Sampling from the mixture
+  for(i in 1:n){
+    if(U[i]< p1){
+      rand.samples[i] = rnorm(1, mu1, s1)
+    }else{
+      rand.samples[i] = rnorm(1, mu2, s2)
+    }
+  }
+  return(rand.samples)
+}
